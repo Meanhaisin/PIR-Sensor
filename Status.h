f830a387afafaa4282aff2074225c5fb31dc7b87
wirano <982593395@qq.com>
2019-06-14 12:20:07 +0800

    add some status

---
+++ Status.h
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+
+#define STATUS_init 0
+#define STATUS_std 1
+#define STATUS_msg 2
+#define STATUS_pair 3
+
+void DO_STATUS_init();
+void DO_STATUS_std();
+void DO_STATUS_msg();
+void DO_STATUS_pair();
